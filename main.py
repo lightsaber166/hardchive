9f6cf0c787027d8cd4b72e6160a3f4e59c98ac60
lightsaber166 <142599447+lightsaber166@users.noreply.github.com>
2023-09-24 17:26:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 import zipfile
-
+import os
 class HardchiveUI:
     def __init__(self, root):
         self.root = root
